39af87f4aeb9fb2cf81677633a0bb47dd090e14a
Tiago-devop <tiagodev@protonmail.com>
2021-05-22 01:52:33 -0300

    adding navbar items

--- cilia-app/src/components/User.js
+++ cilia-app/src/components/User.js
@@ -1,15 +1,34 @@
 import React from "react";
 import "./User.scss";
+import { ReactComponent as CiliaLogo } from "../assets/images/logo.svg";
 
 function User() {
   return (
-    <div>
+    <>
       <div className="navbar">
         <ul className="nav">
-          <i class="material-icons icons--white"> dehaze </i>
+          <li>
+            <i className="material-icons"> dehaze </i>
+          </li>
+          <li>
+            <CiliaLogo />
+          </li>
+          <li>
+            <span className="material-icons">help</span>
+            Ajuda
+          </li>
+          <li>
+            <span className="material-icons">chat</span>
+            Suporte via Chat
+          </li>
+          <li>
+            Diego • Auto/RE - TESTE Administrador
+            <span className="material-icons">account_circle</span>
+            <span className="material-icons">home</span>
+          </li>
         </ul>
       </div>
-    </div>
+    </>
   );
 }
 
